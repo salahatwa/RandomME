bffcb3aad3adf2c63fa356b9d53a4b0f3d46a864
Salah Atwa <Salah Atwa@Salah_Atwa>
2017-01-14 13:43:33 +0200

    first commit change12025

---
+++ src/main/java/com/salahatwa/randomme/ReadedBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.salahatwa.randomme;
+
+/**
+ *
+ * @author Salah Atwa
+ */
+public class ReadedBean {
+   private String cell; 
+
+    /**
+     * @return the cell
+     */
+    public String getCell() {
+        return cell;
+    }
+
+    /**
+     * @param cell the cell to set
+     */
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
+}
